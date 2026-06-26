aec1759feffb5e5d5cf4ec09a078a5332b082fbc
ortiqov_x7 <sheraliortiqboyev4@gmail.com>
2026-06-26 19:00:51 +0500

    updating

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AvtoBotOfficial")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NovixMusic")
         self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/id_uzzz")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
